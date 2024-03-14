ccccceb62fc692d443ba7f5215d5c7fbf61ed50e
mxlq1 <max_libov@mail.ru>
2024-03-14 20:15:53 +0300

    Request file

---
+++ create_dictionary.py
@@ -0,0 +1,16 @@
+'''
+import requests
+from bs4 import BeautifulSoup as BS
+
+r = requests.get("https://klavogonki.ru/vocs/559/")
+html = BS(r.text, 'html.parser')
+
+words = html.find_all("td", class_='text')
+
+with open("dictionary.txt", "w") as f:
+    for word in words:
+        f.write(str(word.text) + "\n")
+'''
+
+
+
